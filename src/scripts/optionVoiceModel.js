c410f38398bc90da67f2030da8b31cb64a1242fc
Adriana Perez <adpc1609@gmail.com>
2018-10-05 00:06:55 -0300

    dictado por voz

--- src/scripts/optionVoiceModel.js
+++ src/scripts/optionVoiceModel.js
@@ -1,6 +1,59 @@
-//import Artyom from '../../node_modules/artyom.js/build/artyom.window';
 
-//const Jarvis = new Artyom();
+const artyom = new Artyom();
 
-//Jarvis.say("Hello World !");
-console.log("model")
+artyom.addCommands([
+    {
+     description:"artyom can talk too,lets say something if we say hello",
+     indexes:["hola","buscar"],
+     action:function(i){
+
+        if(i == 0){
+            artyom.say("hello! How are you?");
+        }
+     }   
+    },
+    { indexes:["adios"],
+    action:function(){
+        alert("now all is over.");
+    }
+  }
+]);
+
+artyom.redirectRecognizedTextOutput(function(text,isFinal){
+    let span = document.getElementById("output");
+    if(isFinal){
+        span.innerHTML = "";
+    }else{
+        span.innerHTML = text;
+    }
+});
+function starArtyom(){
+    var isChecked = document.getElementById('checkbox1').checked;
+    console.log(isChecked);
+     if(isChecked){
+         // Start the commands !
+            artyom.initialize({
+                lang: "es-ES", // GreatBritain english
+                continuous: true, // Listen forever
+                soundex: true,// Use the soundex algorithm to increase accuracy
+                debug: true, // Show messages in the console
+                executionKeyword: "and do it now",
+                listen: true, // Start to listen commands !
+                
+            }).then(() => {
+                artyom.say("hola soy Asistente personal estoy aqui para ayudarte");
+                console.log("Artyom has been succesfully initialized");
+            }).catch((err) => {
+                console.error("Artyom couldn't be initialized: ", err);
+            });
+
+    }else{
+        artyom.fatality().then(() => {
+            // The default configuration is kept
+            // that means spanish mode
+            artyom.say("Adios !");
+        });
+    }
+
+   
+}
